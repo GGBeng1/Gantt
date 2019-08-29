64757c9d3b14e4ce242631239cab8a4d11d6579b
GGbeng <15340019117@163.com>
2019-08-29 15:32:24 +0800

    加入docs及一些设置

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    https: false,
+    port: 9527,
+  },
+  assetsDir: 'static',
+  outputDir: 'docs',
+  publicPath: './'
+};
